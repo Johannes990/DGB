1663e172b5b592ecf14888552a571362bcd0ebb8
johannes990 <johannes.jyrgenson@gmail.com>
2024-04-28 15:34:40 +0300

    prog name changed to DGB

--- src/build.bash
+++ src/build.bash
@@ -1,4 +1,4 @@
 cc -c Main.c -o Main.o
 cc -c world/World.c -o World.o
 
-cc Main.o World.o -o MyProg -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+cc Main.o World.o -o DGB -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
