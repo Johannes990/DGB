0d8f7366c00d1508cfb1d7f91653a0178eb07ad5
johannes990 <johannes.jyrgenson@gmail.com>
2024-04-28 19:32:16 +0300

    cell creation in world

--- src/world/World.c
+++ src/world/World.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
 #include "WorldNode.h"
 #include "World.h"
+#include "../entities/Cell.h"
 
 // world array
-struct WorldNode WORLD_ARRAY[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y];
+struct WorldNode WORLD_BASE_GRID[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y];
 
 // function definitions
 void printWorld(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y], int sizeX, int sizeY) {
@@ -14,11 +17,37 @@ void printWorld(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y], in
     }
 }
 
-void setNodePositions(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y]) {
+void constructWorldBaseGrid(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y]) {
     for (int i = 0; i < WORLD_NODECOUNT_X; i++) {
         for (int j = 0; j < WORLD_NODECOUNT_Y; j++) {
             world[i][j].posX = X_PAD + ((i + 1) * WORLD_NODESPACING);
             world[i][j].posY = Y_PAD + ((j + 1) * WORLD_NODESPACING);
         }
     }
+}
+
+void seedCells(int cellCount, int cellRadius, float clearRadius, int seed) {
+    if (cellCount > WORLD_MAX_ENTITY_COUNT) {
+        errno = EOVERFLOW;
+        return;
+    }
+
+    if (cellRadius < 1) {
+        errno = 
+    }
+
+    // should include some error handling here for when the
+    // clearRadius is taken into account as well
+    for (int i = 0; i < cellCount; i++) {
+        int x = randIntInRange(0, WORLD_NODECOUNT_X);
+        int y = randIntInRange(0, WORLD_NODECOUNT_Y);
+        int r = randIntInRange(1, cellRadius);
+        WORLD_INHABITED_CELLS[i].posX = x;
+        WORLD_INHABITED_CELLS[i].posY = y;
+        WORLD_INHABITED_CELLS[i].radius = 
+    }   
+}
+
+int randIntInRange(int low, int high) {
+    return (rand() % (high - low + 1)) + low;
 }
--- src/world/World.h
+++ src/world/World.h
@@ -11,11 +11,16 @@
 #define WORLD_NODECOUNT_X 25
 #define WORLD_WIDTH ((WORLD_NODESPACING * WORLD_NODECOUNT_X) + (2 * WORLD_NODESPACING) + (2 * X_PAD))
 #define WORLD_HEIGHT ((WORLD_NODESPACING * WORLD_NODECOUNT_Y) + (2 * WORLD_NODESPACING) + (2 * Y_PAD))
+#define WORLD_MAX_ENTITY_COUNT WORLD_NODECOUNT_X * WORLD_NODECOUNT_Y
 
-extern struct WorldNode WORLD_ARRAY[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y];
+// world objects
+extern struct Cell WORLD_INHABITED_CELLS[WORLD_MAX_ENTITY_COUNT];
+extern struct WorldNode WORLD_BASE_GRID[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y];
 
 // function declarations
 void printWorld(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y], int sizeX, int sizeY);
-void setNodePositions(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y]);
+void constructWorldBaseGrid(struct WorldNode world[WORLD_NODECOUNT_X][WORLD_NODECOUNT_Y]);
+void seedCells(int cellCount, int cellRadius, float clearRadius, int seed);
+int randIntInRange(int low, int high);
 
 #endif
