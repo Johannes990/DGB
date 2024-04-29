292ca08e0a0d4c845d2f9a5b08fd88de018aa6ba
johannes990 <johannes.jyrgenson@gmail.com>
2024-04-29 22:28:16 +0300

    bash update

--- src/build.bash
+++ src/build.bash
@@ -1,4 +1,5 @@
+cc -c world_connections/CellConnections.c -o CellConnections.o
 cc -c Main.c -o Main.o
 cc -c world/World.c -o World.o
 
-cc Main.o World.o -o DGB -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+cc Main.o World.o CellConnections.o -o DGB -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
