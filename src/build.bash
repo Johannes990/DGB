12e5d55b44336b496c9c778f93d8ba43c7c30c4b
johannes990 <johannes.jyrgenson@gmail.com>
2024-05-01 01:53:03 +0300

    UtilFunctions added to build srcipt

--- src/build.bash
+++ src/build.bash
@@ -1,5 +1,6 @@
+cc -c globals/UtilFunctions.c -o UtilFunctions.o
 cc -c world_connections/CellConnections.c -o CellConnections.o
 cc -c Main.c -o Main.o
 cc -c world/World.c -o World.o
 
-cc Main.o World.o CellConnections.o -o DGB -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+cc Main.o World.o CellConnections.o UtilFunctions.o -o DGB -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
